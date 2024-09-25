1e056dbd865ae7e94c892b67b41543055286e04a
Gabin Lee <gb94@ewhain.net>
2024-09-25 22:35:20 +0900

    Feat: 내가 보낸 메시지 렌더링

--- src/components/ChatRoomPage/MessageList.tsx
+++ src/components/ChatRoomPage/MessageList.tsx
@@ -1,6 +1,25 @@
-const MessageList = () => {
+import React from 'react';
+
+interface Message {
+    senderId: number;
+    text: string;
+}
+
+interface MessageListProps {
+    messages: Message[];
+    currentUserId: number;
+}
+
+const MessageList: React.FC<MessageListProps> = ({ messages, currentUserId }) => {
     return (
-        <div className="w-full h-messageAreaHeight">
+        <div className="flex flex-col w-full h-messageAreaHeight font-['Pretendard'] px-4 py-2 overflow-y-auto">
+            {messages.map((message, index) => (
+                <div key={index} className={`flex ${message.senderId === currentUserId ? 'justify-end' : 'justify-start'}`}>
+                    <p className={`px-[14px] py-[10px] max-w-[328px] rounded-[20px] break-all my-1 ${message.senderId === currentUserId ? 'bg-Purple/1 text-White' : 'bg-White text-black'}`}>
+                        {message.text}
+                    </p>
+                </div>
+            ))}
         </div>
     );
 }
--- src/pages/ChatRoomPage.tsx
+++ src/pages/ChatRoomPage.tsx
@@ -20,7 +20,7 @@ const ChatRoomPage = () => {
         <div className='w-width h-height bg-Purple/3 relative'>
             <TopBar />
             <Header />
-            <MessageList />
+            <MessageList messages={messages} currentUserId={currentUser.userId}/>
             <div className='absolute bottom-0 w-width'>
                 <ChatBar onSendMessage={handleSendMessage}/>
                 <HomeIndicator />
